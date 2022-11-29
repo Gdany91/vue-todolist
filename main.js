32f316b8b4ec5fd0f4934c2d4d7fb2cc14a70b2d
gdany91 <you@example.comdgarofalo52@gmail.com>
2022-11-29 16:04:54 +0100

    markup

---
+++ main.js
@@ -0,0 +1,22 @@
+const { createApp } = Vue;
+
+createApp({
+    data(){
+        return{
+            error: false,
+            newTask:'',
+            tasks: [
+                { text: 'Fare i compiti', done: false }, 
+                { text: 'Fare la spesa', done: true }, 
+                { text: 'Fare il bucato', done: false }
+                ]
+        },
+
+    methods: {
+
+        addItem(){
+
+        }
+    }
+    }
+}).mount(#app)
