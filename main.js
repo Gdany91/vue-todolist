15bccf6ceda005764665b7621682a45e85c6d8f2
gdany91 <you@example.comdgarofalo52@gmail.com>
2022-11-29 16:43:18 +0100

    add-item

--- main.js
+++ main.js
@@ -9,14 +9,23 @@ createApp({
                 { text: 'Fare i compiti', done: false }, 
                 { text: 'Fare la spesa', done: true }, 
                 { text: 'Fare il bucato', done: false }
-                ]
-        },
+            ]
+        }
+        
+    },
 
     methods: {
 
         addItem(){
+            if( this.newTask.length < 5 || this.newTask === ''){
+                 
+            }
+
+            else {
+                this.tasks.unshift(this.newTask)
+            }
 
         }
     }
-    }
-}).mount(#app)
+    
+}).mount('#app')
